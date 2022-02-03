1a8faa0360b6f3d9117044fba2a557145808b961
AshishSingh159357 <as3861425@gmail.com>
2022-02-03 17:28:36 +0530

    second-commit

--- src/main/scala/Day1Assignment/FileReadFromHdfs.scala
+++ src/main/scala/Day1Assignment/FileReadFromHdfs.scala
@@ -11,10 +11,19 @@ object FileReadFromHdfs {
       .config("spark.history.fs.logDirectory","file:////usr/local/spark-3.2.1-bin-hadoop3.2/Extra-Spark/sprak-events")
       .getOrCreate()
 
+    // To Load the the data
     var df= sparkSession.read.format("csv").option("header","true").load("hdfs://localhost:9000/dataset/employee_address_details.csv")
-    df.write.mode("overwrite").csv("hdfs://localhost:9000/dataset/New_employee_address_details.csv")
-    df.show()
 
+    // Storing Data with Different Name
+    // overwrite mode is to replace the file and create automatic given path on hadoop
+    df.write.mode("overwrite").csv("hdfs://localhost:9000/dataset/New_employee_address_details.csv")
+
+    // Display 1000 rows and full column content
+    df.show(1000,false)
+
+
+
+    // Filter the data with given condition
     val df1=df.filter(df("Region")==="Northeast")
 
     val df2=df.filter(df("Region")==="South")
@@ -25,24 +34,23 @@ object FileReadFromHdfs {
 
     val df5=df.filter(df("Region")==="East")
 
+    //To show the DataFrame
     df1.show()
     df2.show()
     df3.show()
     df4.show()
     df5.show()
-   // df=df.filter(df("Place_Name")==="Sodus")
-   // df.show()
+
+    // This will store data of dataframe to HDFS to given path
     df1.write.mode("overwrite").csv("hdfs://localhost:9000/dataset/empdata/Region=NorthEast/NorthEast.csv")
     df2.write.mode("overwrite").csv("hdfs://localhost:9000/dataset/empdata/Region=South/South.csv")
     df3.write.mode("overwrite").csv("hdfs://localhost:9000/dataset/empdata/Region=Midwest/Midwest.csv")
     df4.write.mode("overwrite").csv("hdfs://localhost:9000/dataset/empdata/Region=West/West.csv")
     df5.write.mode("overwrite").csv("hdfs://localhost:9000/dataset/empdata/Region=East/East.csv")
 
+    //This will Partition or Store the data Region Wise in HDFS to given path
+    df.write.option("header",true).partitionBy("Region").mode("overwrite").csv("hdfs://localhost:9000/dataset/empdata-2/Region")
 
-
-
-   // val dff= sparkSession.read.format("csv").option("header","true").load("hdfs://localhost:9000/dataset/Test_address_details.csv")
-   // dff.show()
 
   }
 
--- src/main/scala/Day2Assignment/SparkSql.scala
+++ src/main/scala/Day2Assignment/SparkSql.scala
@@ -22,32 +22,46 @@ object SparkSql {
     var P1= sparkSession.read.format("csv").option("header","true")
       .load("hdfs://localhost:9000/dataset/Employee_personal_details.csv")
 
-  //  B1.show()
-    //P1.show()
 
+    // Creating Temp View For Business Details
     B1.createOrReplaceTempView("EBD")
+
+    // Creating Temp View For Personal Details
     P1.createOrReplaceTempView("EPD")
 
 
 
+    // Display average ,minimum ,maximum salary for employee between 30 and 40
+    var sql1 = sparkSession.sql("SELECT avg(salary) as AverageSalary,min(salary) as MinSalary,max(salary) as MaxSalary FROM EPD inner join EBD on EPD.Emp_ID=EBD.Emp_ID where EPD.AgeinYrs between 30 and 40")
+    sql1.show()
 
 
-  var sql1 = sparkSession.sql("SELECT * FROM EBD")
- sql1.show()
-
-    var sql2= sparkSession.sql("SELECT * FROM EPD")
-   sql2.show()
-
-
-
-
+    // Display number of employee join at particular year
+    var sql2 = sparkSession.sql("SELECT count(Emp_ID),Year_of_Joining FROM EBD group by Year_of_Joining order by Year_of_Joining")
+    sql2.show()
 
 
 
-    // df=df.filter(df("Place_Name")==="Sodus")
-    // df.show()
-    // val dff= sparkSession.read.format("csv").option("header","true").load("hdfs://localhost:9000/dataset/Test_address_details.csv")
-    // dff.show()
+
+
+
+
+
+   // var sql3 = sparkSession.sql("SELECT count(Emp_ID),Year_of_Joining FROM EBD group by Year_of_Joining order by Year_of_Joining")
+ //B1.collect()
+
+   // print("----------------------------------------")
+
+
+
+   // B1.foreach(row=>{
+    //  var a=row.getAs("Salary")
+    //  print(a.asInstanceOf[Int]+4)
+
+
+   // })
+
+
 
   }
 
